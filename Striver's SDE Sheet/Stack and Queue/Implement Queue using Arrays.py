c538d7cd9bada37e6492974d59cf5cedfc04fc95
Yash Honrao <yashhonrao2024@gmail.com>
2026-03-14 16:53:50 -0500

    Completed Implement Queue using Arrays

---
+++ Striver's SDE Sheet/Stack and Queue/Implement Queue using Arrays.py
@@ -0,0 +1,15 @@
+class ArrayQueue:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x):
+        self.queue.append(x)
+
+    def pop(self):
+        return self.queue.pop(0)
+
+    def peek(self):
+        return self.queue[0]     
+
+    def isEmpty(self):
+        return not self.queue

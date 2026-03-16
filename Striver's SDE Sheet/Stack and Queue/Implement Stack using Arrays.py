af1f4c796f46778a4fb8ff87d66897cc2540349d
Yash Honrao <yashhonrao2024@gmail.com>
2026-03-16 13:02:22 -0500

    Edited Implement Stack using Arrays

--- Striver's SDE Sheet/Stack and Queue/Implement Stack using Arrays.py
+++ Striver's SDE Sheet/Stack and Queue/Implement Stack using Arrays.py
@@ -22,9 +22,55 @@ class ArrayStack:
     def clearStack(self):
         self.stack = []
 
+""" The above is my implementation of the stack using arrays.
+    But according to Striver, we shouldn't use the .pop() methods and all, and it should be purely using arrays and pointers.
+    But, we need to use a fixed sizr array in this case, which we can ask the interviewer what size of array can I consider.
+"""
+class ImplementStackUsingArrays:
+    def __init__(self):
+        self.stack = [None] * 10
+        self.top = -1
 
-obj = ArrayStack()
+    def push(self, x):                    # Time Complexity: O(1)
+        if self.top >= len(self.stack) - 1:
+            return "Stack is full"
+        self.top += 1
+        self.stack[self.top] = x
+
+    def pop(self):                        # Time Complexity: O(1)
+        if self.top == -1:
+            return None
+        res = self.stack[self.top]
+        self.stack[self.top] = None
+        self.top -= 1
+        return res
+
+    def top(self):                        # Time Complexity: O(1)
+        if self.top == -1:
+            return None
+        return self.stack[self.top]
+
+    def isEmpty(self):                    # Time Complexity: O(1)
+        if self.top == -1:
+            return True
+        return False
+
+    def size(self):                        # Time Complexity: O(1)
+        return self.top + 1
+
+    def printStack(self):                    # Time Complexity: O(1)
+        return self.stack[:self.top + 1]
+
+    def clearStack(self):
+        self.stack = [None] * 10
+
+    # Space Complexity: O(N)
+
+obj = ImplementStackUsingArrays()
 print(obj.isEmpty())
 obj.push(1)
 print(obj.pop())
 print(obj.isEmpty())
+print(obj.printStack())
+obj.clearStack()
+print(obj.printStack())

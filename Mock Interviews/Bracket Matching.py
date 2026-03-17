d66f19328288add8308189a718f13fa00d850228
Yash Honrao <yashhonrao2024@gmail.com>
2026-03-16 21:02:56 -0500

    Mock Test Bracket Matching

---
+++ Mock Interviews/Bracket Matching.py
@@ -0,0 +1,46 @@
+""" Mock Interview; 16th March, 2026. with Adesh on Exponent """
+"""
+Bracket Match
+A string of brackets is considered correctly matched if every opening bracket in the string can be paired up with a later closing bracket, and vice versa. For instance, “(())()” is correctly matched, whereas “)(“ and “((” aren’t. For instance, “((” could become correctly matched by adding two closing brackets at the end, so you'd return 2.
+
+Given a string that consists of brackets, write a function bracketMatch that takes a bracket string as an input and returns the minimum number of brackets you'd need to add to the input in order to make it correctly matched.
+
+Explain the correctness of your code, and analyze its time and space complexities.
+
+
+Examples:
+input:  text = “(()”
+output: 1
+
+input:  text = “(())”
+output: 0
+
+input:  text = “())(”
+output: 2
+
+Constraints:
+
+- [time limit] 5000ms
+
+- [input] string text
+
+-1 ≤ text.length ≤ 5000
+- [output] integer
+"""
+
+def bracket_match(text: str) -> int:
+    opening = 0
+    unmatched = 0
+
+    for bracket in text:
+        if bracket == "(":
+            opening += 1
+        elif bracket == ")":
+            if opening > 0:
+                opening -= 1
+            else:
+                unmatched += 1
+    return opening + unmatched
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    # where n is the length of the text
